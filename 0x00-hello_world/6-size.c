00668899cacf9fb015e359ff206868b2da345e4b
attanseye <attanseye@gmail.com>
2022-09-08 16:24:01 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 
 	printf("Size of a char: 1 byte(s)\n", sizeof(a));
 	printf("Size of an int: 4 byte(s)\n", sizeof(b));
-	printf("Size of a long int: 4 byte(s)\n", sizeof(c));
+	printf("Size of a long int: 8 byte(s)\n", sizeof(c));
 	printf("Size of a long long int: 8 byte(s)\n", sizeof(d));
 	printf("Size of a float: 4 byte(s)\n", sizeof(e));
 	return (0);
