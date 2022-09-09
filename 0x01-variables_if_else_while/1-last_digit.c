1fa837466b90a79d4dada3672f0f52cd8d7e5391
attanseye <attanseye@gmail.com>
2022-09-09 09:31:55 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 
-/**
+ /**
  * main - print if the number is postive, zero, or negative
  *
  * Description: using the main function
@@ -15,7 +15,7 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-  /* your code goes there */
+/* your code goes there */
 if (n > 0)
 {
 	printf("%i is positive\n", n);
