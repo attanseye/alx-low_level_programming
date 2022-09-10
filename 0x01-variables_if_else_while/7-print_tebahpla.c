a399b1de2b2db0af7a6edd178953dbc7889e3908
attanseye <attanseye@gmail.com>
2022-09-10 14:51:01 -0700

    smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,22 +1,20 @@
- #include <stdio.h>
+#include <stdio.h>
+
 /**
- * main - printing numbers from 0-9 with commas and space between them
- * Description: using the main function
- * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
- * Return: 0
+ * main - Prints the alphabet at reverse
+ *
+ * Return: Always (Success)
  */
 int main(void)
 {
-int c;
-for (c = 48; c <= 57; c++)
+char c;
+
+for (c = 'z'; c >= 'a'; c--)
 {
-	putchar(c);
-if (c != 57)
-{
-	putchar(',');
-	putchar(' ');
+putchar(c);
 }
-}
+
 putchar('\n');
+
 return (0);
 }
