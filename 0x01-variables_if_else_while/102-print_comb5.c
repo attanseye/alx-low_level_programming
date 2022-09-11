7ac4bdd0f44947c782652e592d9de36aa61ff884
attanseye <attanseye@gmail.com>
2022-09-11 04:02:53 -0700

    software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,14 +12,16 @@ int i, p;
 
 for (i = 0; i < 100; i++)
 {
-for (p = 0; p < 100; j++)
+for (p = 0; p < 100; p++)
 {
 	
 if (p > i)
 {
 putchar((i / 10) + '0');
-putchar((i % 10) + '0');							putchar(' ');
-putchar((p / 10) + '0');							putchar((p % 10) + '0');
+putchar((i % 10) + '0');
+putchar(' ');
+putchar((p / 10) + '0');							
+putchar((p % 10) + '0');
 if (i != 98)
 {
 putchar(',');
