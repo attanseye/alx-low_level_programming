88fdd1da6643663ef793f38b27aaaffa56163230
attanseye <attanseye@gmail.com>
2022-09-16 02:06:46 -0700

    task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,4 +1,4 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
  * main - prints sum of even fibonacci
  * suit elements
@@ -7,15 +7,17 @@ nclude <stdio.h>
  */
 int main(void)
 {
-unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
-while (n3 <= 4000000)
-{
-n3 = n1 + n2;
-n1 = n2;
-n2 = n3;
-if ((n1 % 2) == 0)
-sum += n1;
-}
-printf("%ld\n", sum);
-return (0);
+	unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
+	while (n3 <= 4000000)
+	{
+		n3 = n1 + n2;
+		n1 = n2;
+		n2 = n3;
+
+		if ((n1 % 2) == 0)
+			sum += n1;
+
+	}
+	printf("%ld\n", sum);
+	return (0);
 }
