7326ccfb01c6218c7ee330007a8d6cc283951ef7
attanseye <attanseye@gmail.com>
2022-09-22 14:34:39 -0700

    task o

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,12 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Auth: emmanuel attansey
- * Desc: Header file containing prototypes for all functions
- * used in the 0x06.C - More pointers, arrays and strings directory.
- */
+#include <stdio.h>
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -19,7 +14,5 @@ char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
 
-#endif
+#endif /* MAIN_H */
