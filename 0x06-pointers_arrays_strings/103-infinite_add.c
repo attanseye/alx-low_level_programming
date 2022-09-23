0d8140b8b19e81b4d6c1566988d7e4d32f911284
attanseye <attanseye@gmail.com>
2022-09-23 13:20:30 -0700

    tasks 10-11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  * infinite_add - adds two numbers
@@ -23,12 +23,17 @@ m = 0;
 for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 {
 n = m;
-if (i >= 0)									n += n1[i] - '0';
+if (i >= 0)
+n += n1[i] - '0';
 if (j >= 0)
-n += n2[j] - '0';								if (i < 0 && j < 0 && n == 0)
-{										break;										}
+n += n2[j] - '0';
+if (i < 0 && j < 0 && n == 0)
+{
+break;
+}
 m = n / 10;
-r[k] = n % 10 + '0';								}
+r[k] = n % 10 + '0';
+}
 r[k] = '\0';
 if (i >= 0 || j >= 0 || m)
 return (0);
