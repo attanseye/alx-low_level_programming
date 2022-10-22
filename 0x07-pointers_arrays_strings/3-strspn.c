b1470eac16f2ab195e98dd019c9a0313a5d8e430
attanseye <attanseye@gmail.com>
2022-10-22 10:31:25 -0700

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,16 +13,19 @@ unsigned int a = 0, b, t = 0;
 
 while (accept[a])
 {
-b = 0;
-										while (s[b] != 32)
-{
-if (accept[a] == s[b])
-										{
-										t++;
-										}
-b++;
-}
-a++;
+	b = 0;
+
+	while (s[b] != 32)
+	{
+		if (accept[a] == s[b])
+		{
+			t++;
+		}
+		
+		b++;
+	}
+
+	a++;
 }
 
 return (t);
